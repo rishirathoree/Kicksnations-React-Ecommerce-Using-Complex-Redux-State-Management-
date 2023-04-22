eb55fda608260ad2aed1fd7ed3ac251acc025bb9
rishirathoree <rishirathoree3@gmail.com>
2023-04-22 08:37:55 +0530

    few page added

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,7 @@ import Cart from "./Pages/Cart";
 import { Footwear } from "./Pages/Footwear";
 import { Girls } from "./Pages/Girls";
 import { Home } from "./Pages/Home";
-<<<<<<< HEAD
-import { NoPagefound } from "./Pages/NoPagefound";
-=======
-import Nopagefound from "./Pages/Nopagefound";
-
-
->>>>>>> 70e48a884cb0e851cb198c18bef75c24b4dbdc84
+import { Pagerror } from "./Pages/Pagerror";
 import Product from "./Pages/Product";
 import { Slides } from "./Pages/Slides";
 import Navbar from "./components/Navbar";
@@ -21,23 +15,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Basketball" element={<Basketball />} />
-        <Route path="/Footwear" element={<Footwear />} />
-        <Route path="/Girls" element={<Girls />} />
-        <Route path="/Slides" element={<Slides />} />
-        <Route path="/Cart" element={<Cart />} />
-        <Route path="/Apparel" element={<Apparel />} />
-        <Route path="*" element={<NoPagefound />} />
-        <Route path="/Accessories" element={<Accessories />} />
-        <Route path="/Products/:id" element={<Product />} />
-        
-      </Routes>
-      
-    </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Basketball" element={<Basketball />} />
+          <Route path="/Footwear" element={<Footwear />} />
+          <Route path="/Girls" element={<Girls />} />
+          <Route path="/Slides" element={<Slides />} />
+          <Route path="/Cart" element={<Cart />} />
+          <Route path="/Apparel" element={<Apparel />} />
+          <Route path="/Accessories" element={<Accessories />} />
+          <Route path="*" element={<Pagerror />} />
+          <Route path="/Products/:id" element={<Product />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/Pages/Pagerror.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import jordans from "../images/jordans.png"
+import { useNavigate } from 'react-router-dom'
+export const Pagerror = () => {
+    const navigate = useNavigate()
+    return (
+      <>
+      <div className='lg:mt-[116px] md:mt-[116px] sm:mt-[92px]'>
+          <div className='w-full flex justify-center flex-col items-center h-screen'>
+              <img src={jordans} className='w-60 animate-bounce h-40' alt="" />
+              <p className='font-lighter  text-xsm'>404 Page Found</p>
+              <button
+              onClick={()=>{navigate('/')}}
+              className='bg-black font-light text-white uppercase text-xsm px-12 py-2  mt-4'>go back</button>
+          </div>
+      </div>
+      </>
+      )
+}
