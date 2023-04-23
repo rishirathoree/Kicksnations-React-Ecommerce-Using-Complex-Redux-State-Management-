d5a10f96d063d6d9cdf01637b06cc2d0bb87fcc6
rishirathoree <rishirathoree3@gmail.com>
2023-04-23 09:41:40 +0530

    account page eidted

--- src/Pages/Accountpage.jsx
+++ src/Pages/Accountpage.jsx
@@ -19,19 +19,23 @@ const Accountpage = () => {
   return (
     <div className='lg:mt-[116px] md:mt-[92px] flex items-center p-8 flex-col  justify-center w-full h-full sm:mt-[92px]'>
         <div className='w-full border rounded-2xl grid overflow-hidden lg:grid-cols-2 md:grid-cols-1 h-full'>
-        <div className='overflow-hidden'>
+        <div className='w-full h-full overflow-hidden'>
             <img src={bg} alt="" />
         </div>
-        <div className='flex md:my-20 sm:my-20 justify-center flex-col items-center'>
+        <div className='flex md:my-20 sm:my-4 justify-center flex-col items-center'>
         <p className='font-bold text-bold uppercase text-left'>account details</p>
         <div className='w-20 h-20 my-4 overflow-hidden rounded-full'>
             <img src={dp} alt="" />
         </div>
             <p className='font-light text-xsm'>{user.email}</p>
+            <span 
+            onClick={handleLogout}
+            className='font-light cursor-pointer text-xsm bg-black text-white my-2 p-2'>Logout</span>
+            <span 
+            onClick={()=>{navigate('/')}}
+            className='font-light cursor-pointer text-xsm bg-black text-white my-2 p-2'>Go To Home</span>
         </div>
         </div>
-        {/* <p>email:{user.email}</p>
-        <button onClick={handleLogout} className='text-sm bg-black text-white p-2'>Log out</button> */}
     </div>
   )
 }
